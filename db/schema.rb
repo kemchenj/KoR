5c5af9682e6fff196d6f138cc0eb2002dacc3186
kemchenj <chenkem95@outlook.com>
2018-03-20 23:15:55 +0800

    feat: Build Store Product Views

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20180320142440) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
