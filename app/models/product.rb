086bdfab33ed526dd8a997474a90631a9a94797f
kemchenj <chenkem95@outlook.com>
2018-03-22 20:34:47 +0800

    feat: add custom error message for product title validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
   validates_presence_of :title, :description, :image_url, :price
   validates_uniqueness_of :title
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, length: { minimum: 10, too_short: 'length was too short' }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
