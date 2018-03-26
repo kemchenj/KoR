2b5ef74d12f91d51ae837d414a4057b0a5e83807
kemchenj <chenkem95@outlook.com>
2018-03-27 07:57:22 +0800

    feat: Add Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  # 数据验证
   validates_presence_of :title, :description, :image_url, :price
   validates_uniqueness_of :title
   validates :title, length: { minimum: 10, too_short: 'length was too short' }
@@ -7,4 +12,14 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  private 
+
+  # 确保仍有商品在引用此产品 
+  def ensure_not_referenced_by_any_line_item 
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
