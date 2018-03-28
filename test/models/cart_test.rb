51426bc9a591cddd544b955920586fcecb5bb561
kemchenj <chenkem95@outlook.com>
2018-03-29 04:37:39 +0800

    feat: add unit test to cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "cart should create a new line item when adding a new proudct" do
+    cart = carts(:only_ruby)
+
+    cart.add_product(products(:two))
+
+    assert_equal cart.line_items.to_a.count, 2
+  end
+
+  test "cart should update existing line item quantity when adding an existing product" do
+    cart = carts(:only_ruby)
+
+    cart.add_product(products(:ruby)).save!
+
+    assert_equal cart.line_items.to_a.size, 1
+  end
 end
