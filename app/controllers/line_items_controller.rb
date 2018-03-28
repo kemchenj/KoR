ffbfad2cc3f69383b9163fe0df5bc53d75521f70
kemchenj <chenkem95@outlook.com>
2018-03-29 04:49:57 +0800

    feat: add line item delete button

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -59,8 +59,9 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
+
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
