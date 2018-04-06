9e09d316219ef82402ad9f29669864ac2452bd1f
kemchenj <chenkem95@outlook.com>
2018-04-06 22:17:59 +0800

    fix: 修复 line_item 固件问题导致的测试失败

--- db/migrate/20180328133833_add_price_to_line_items.rb
+++ db/migrate/20180328133833_add_price_to_line_items.rb
@@ -2,8 +2,6 @@ class AddPriceToLineItems < ActiveRecord::Migration[5.1]
   def up
     add_column :line_items, :price, :decimal, precision: 8, scale: 2
 
-    LineItem.reset_column_information
-
     LineItem.all.each do |item|
       item.update_attribute :price, item.product.price
     end
